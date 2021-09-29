727c747be7ddade1d204c1eb04ea01c5c20cb6a3
Adam Rolain <AdamRolain@QuickenLoans.com>
2021-09-29 16:16:09 -0400

    Completed all GET requests.

--- Lab12_2MoviesAPI/Controllers/MovieController.cs
+++ Lab12_2MoviesAPI/Controllers/MovieController.cs
@@ -12,18 +12,60 @@ namespace Lab12_2MoviesAPI.Controllers
     [ApiController]
     public class MovieController : ControllerBase
     {
-        // https://localhost:44359/api/Movie
+        // GET https://localhost:44359/api/Movie
         [HttpGet]
         public List<Movie> GetAll()
         {
             return DAL.GetAllMovies();
         }
 
-        // https://localhost:44359/api/Movie/Category?catId=SCIFI
+        // GET https://localhost:44359/api/Movie/Category?catId=SCIFI
         [HttpGet("Category")]
         public List<Movie> GetMoviesByCategory(string catId)
         {
             return DAL.GetMoviesByCategory(catId);
+        }
+
+        // GET https://localhost:44359/api/Movie/Random
+        [HttpGet("Random")]
+        public Movie GetRandomMovie()
+        {
+            return DAL.GetRandomMovie();
+        }
+
+        // GET https://localhost:44359/api/Movie/Random/HIST
+        [HttpGet("Random/Category/{catId}")]
+        public Movie GetRandomMovieInCategory(string catId)
+        {
+            return DAL.GetRandomMovieinCategory(catId);
+        }
+
+        // GET https://localhost:44359/api/Movie/Random/8
+        [HttpGet("Random/List{numberOfMovies}")]
+        public List<Movie> GetRandomMovieList(int numberOfMovies)
+        {
+            return DAL.GetRandomMovieList(numberOfMovies);
+        }
+
+        // GET https://localhost:44359/api/Movie/Categories
+        [HttpGet("Categories")]
+        public List<string> GetAllCategories()
+        {
+            return DAL.GetAllCategories();
+        }
+
+        // GET https://localhost:44359/api/Movie/Title?movieTitle=inception
+        [HttpGet("Title")]
+        public Movie GetMovieDetails(string movieTitle)
+        {
+            return DAL.GetMovieByTitle(movieTitle);
+        }
+
+        // GET https://localhost:44359/api/Movie/Search?searchString=king
+        [HttpGet("Search")]
+        public List<Movie> GetMovieListBySearch(string searchString)
+        {
+            return DAL.GetMovieByTitleSearch(searchString);
         }
     }
 }
--- Lab12_2MoviesAPI/Models/DAL.cs
+++ Lab12_2MoviesAPI/Models/DAL.cs
@@ -21,5 +21,80 @@ namespace Lab12_2MoviesAPI.Models
         {
             return DB.Query<Movie>("SELECT * FROM movie WHERE category = @categoryId", new { categoryId = categoryId }).ToList();
         }
+
+        public static Movie GetMovieById(int id)
+        {
+            return DB.Get<Movie>(id);
+        }
+
+        public static Movie GetRandomMovie()
+        {
+            Random random = new Random();
+            var maxValue = DB.Query<int>("SELECT COUNT(id) FROM movie");
+            int randomId = random.Next(1, maxValue.First() + 1);
+
+            return GetMovieById(randomId);
+        }
+
+        public static Movie GetRandomMovieinCategory(string catId)
+        {
+            List<Movie> moviesInCategory = GetMoviesByCategory(catId);
+            Random random = new Random();
+            int randomIdInList = random.Next(moviesInCategory.Count);
+            int id = moviesInCategory[randomIdInList].Id;
+
+            return GetMovieById(id);
+        }
+
+        public static List<Movie> GetRandomMovieList(int numberOfMovies)
+        {
+            List<Movie> randomMovies = new List<Movie>();
+
+            while (numberOfMovies > 0)
+            {
+                bool movieOnList = false;
+                Movie currentMovie = DAL.GetRandomMovie();
+
+                foreach (Movie movie in randomMovies)
+                {
+                    if (movie.Id == currentMovie.Id)
+                    {
+                        movieOnList = true;
+                        break;
+                    }
+                }
+                
+                if (!movieOnList)
+                {
+                    randomMovies.Add(currentMovie);
+                    numberOfMovies--;
+                }
+            }
+
+            return randomMovies;
+        }
+
+        public static List<string> GetAllCategories()
+        {
+            return DB.Query<string>("SELECT DISTINCT category FROM movie").ToList();
+        }
+
+        public static Movie GetMovieByTitle(string movieTitle)
+        {
+            List<Movie> queryResult = DB.Query<Movie>("SELECT * FROM movie WHERE name = @movieTitle", new { movieTitle = movieTitle}).ToList();
+            if (queryResult.Count > 0)
+            {
+                return queryResult[0];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static List<Movie> GetMovieByTitleSearch(string searchString)
+        {
+            return DB.Query<Movie>("SELECT * FROM movie WHERE name LIKE @searchString", new { searchString = $"%{searchString}%" }).ToList();
+        }
     }
 }
